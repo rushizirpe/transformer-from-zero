cb59de2c0255eff467868bd0b3eb660c8e2171e4
Rishi <62651883+rushizirpe@users.noreply.github.com>
2023-12-14 11:40:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -35,3 +35,16 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False)
         return self.dropout(x)
+
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 10**6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))    # Multiplied
+        self.bias = nn.Parameter(torch.zeros(1))    # Added
+
+    def forward(self, x):
+        mean = x.mean(dim = -1, keepdim = True)
+        std = x.std(dim = -1, keepdim = True)
+        return self.alpha * (x-mean) / (std + self.eps) + self.bias
+
