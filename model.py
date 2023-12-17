364d0dc1bff0d2720a96ee128402d5c487323b23
Rishi <62651883+rushizirpe@users.noreply.github.com>
2023-12-17 16:39:44 +0530

    Added Feed forward Block

--- model.py
+++ model.py
@@ -48,3 +48,14 @@ class LayerNormalization(nn.Module):
         std = x.std(dim = -1, keepdim = True)
         return self.alpha * (x-mean) / (std + self.eps) + self.bias
 
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super().__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)    # W1 and B1
+        self.linear_2 = nn.Linear(d_ff, d_model)    # W2 and B2
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # Batch, seq_len, d_model)  --> (Batch, seq_len, d_ff)  --> (Batch, seq_len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
