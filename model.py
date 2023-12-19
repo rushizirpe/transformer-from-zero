c3d1b87625a00d061591ba4b958d757f1dc2d83b
Rishi <62651883+rushizirpe@users.noreply.github.com>
2023-12-19 15:15:15 +0530

    Added Residual Connection

--- model.py
+++ model.py
@@ -108,4 +108,11 @@ class MultiHeadAttentionBlock(nn.Module):
 
         return self.w_o(x)
 
+class ResidualConnection(nn.Module):
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
 
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
