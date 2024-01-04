15bf4ee55103eab119375ff8b9167c3c81e4fa60
Rishi <rushikesh.zirpe1234@gmail.com>
2024-01-04 12:17:12 +0530

    Added Model Training Script

---
+++ train.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+import os
+
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+from pathlib import Path
+
+def get_tokenizer(config, dataset, language):
+    tokenizer_path = Path(config['tokenizer_file'].format(language))
+
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token = '[UNK]'))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens = ["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency = 2)
+        tokenizer.train_from_iterator(get_examples(dataset, language), trainer = trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
