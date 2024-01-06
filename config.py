0478f30740cefbe2cf42950e2df75a7c756786de
Rishi <rushikesh.zirpe1234@gmail.com>
2024-01-06 19:12:24 +0530

    Defined Configuration

---
+++ config.py
@@ -0,0 +1,16 @@
+
+def get_config():
+    return {
+        "batch_size": 8,
+        "num_epochs": 2,
+        "lr": 10**-4,
+        "seq_len": 350,
+        "d_model": 512,
+        "lang_src": "en",
+        "lang_tgt": "it",
+        "model_folder": "weights",
+        "model_basename": "tmodel_",
+        "preload": None,
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel"
+    }
