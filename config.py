20ccb3725ce3f53f18b8afc99617551514a07d27
Rishi <rushikesh.zirpe1234@gmail.com>
2024-01-06 19:13:53 +0530

    Added Preloading of Model Weights

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 
 def get_config():
     return {
@@ -14,3 +15,10 @@ def get_config():
         "tokenizer_file": "tokenizer_{0}.json",
         "experiment_name": "runs/tmodel"
     }
+
+def get_weights_file_path(config, epoch: str):
+    model_folder = config["model_folder"]
+    model_basename = config["model_basename"]
+    model_filename = f"{model_basename}{epoch}.pt"
+
+    return str(Path(".") / model_folder / model_filename)
